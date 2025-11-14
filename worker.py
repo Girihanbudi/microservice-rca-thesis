0a6567e47a44e9258fc8a492a93300a91ef187f2
girihanbudi <Ghanbudi@gmail.com>
2025-11-14 14:21:36 +0700

    add version output

--- worker.py
+++ worker.py
@@ -58,6 +58,7 @@ def handler(event):
 
     # Return structured logs + full file listing
     return {
+        "version": "1.0.0",
         "command": " ".join(shlex.quote(c) for c in command),
         "return_code": process.returncode,
         "stdout": process.stdout,
